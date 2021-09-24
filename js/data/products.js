80449ceff9edba8ccc6e61e1f81ac99615652ec7
Yana <yanaegorova69.3@gmail.com>
2021-09-24 20:42:56 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,161 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Computer Mouse Pad',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Gaming mouse pad—ideal for gamers, graphic designers, or anyone who uses a mouse for long sessions.',
+            },
+            {
+                text: 'High-quality cloth surface promotes smooth mouse gliding and enhanced precision.',
+            },
+            {
+                text: 'Steady, thick, rubberized base keeps mouse pad in place.',
+            },
+            {
+                text: 'Hand wash only.',
+            },
+            {
+                text: 'Measures 12.4 x 10.6 x 0.08 inches (314.96 x 269.24 x 2.032 mm).',
+            },
+            {
+                text: 'Note: Mouse pad is transported rolled up so we recommend that it be laid flat under a heavy object for 24 hours to remain flat.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Drink Coaster Set',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'Stylish Natural Wooden Patterns',
+                text: 'They are made of natural acacia wood, with natural Wooden patterns, so it can uses as coasters as well as decorations on the tables.',
+            },
+            {
+                title: 'Protection Functions',
+                text: 'These coaster are ideal for protecting tables and counters from condensation on beverage glasses. Coasters have feet to avoid damage to surfaces and to prevent sliding.',
+            },
+            {
+                title: 'Being Very Useful',
+                text: 'They can be used with a long neck bottle of beer, a relaxing glass of wine, an ice-cold soda in your favorite cup, or a hot cup of coffee, to keep your tables protected from water damage or heat.',
+            },
+            {
+                title: 'Being Durable And Easy To Clean',
+                text: 'They are made of top level acacia wood, they would be very durable. They are easy to clean, only clean with damp cloth and towel to dry.',
+            },
+            {
+                title: 'Warm Tips',
+                text: 'Because the coaster is made of natural acacia, with natural deviations in its appearance. Please be aware that every coaster has its unique patterns.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Front Tire Assembly',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Tire: 2 pack 15x6. 00-6 tube-type turf tread - pneumatic 4 Ply rating.',
+            },
+            {
+                text: 'Wheels: Solid dark gray steel, 3" Centered hub, 3/4" Sintered iron bushings.',
+            },
+            {
+                text: 'Load capacity/speed: 400 lbs. / not for highway speeds.',
+            },
+            {
+                text: 'Use: replacement for  riding mowers with front tire size 15x6. 00-6.',
+            },
+            {
+                text: 'Low hassle, low cost solution to repairing or replacing your  riding Mower tires.',
+            },
+            {
+                text: 'Check your existing tire sidewall for tire, hub, and bushing size to ensure proper replacement fit.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Digital Microwave Oven',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Pre-programmed buttons for popcorn, potato, pizza, frozen vegetable, beverage and dinner plate.',
+            },
+            {
+                text: '900 Watts with 10 power settings, clock and kitchen timer, 30 seconds express cooking.',
+            },
+            {
+                text: 'Large LED digital display, easy-to-read control panel, child safety lock, interior light, removable 10.6” glass turntable.',
+            },
+            {
+                text: 'Push-button door release for easy access.',
+            },
+            {
+                text: 'Dimensions (W x D x H): 19.1” x 14.8” x 11.5”, Internal dimensions: 12.36” x 13.66” x 8.70” (W x D x H).',
+            },
+            {
+                text: 'UL approved. One year limited warranty.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Drill Bit Sharpener',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Sharpen Multiple Types Of Drill Bits',
+                text: 'High-speed steel, masonry, cobalt, & TiN-coated drill bits. Designed for the serious tool user, this professional, easy-to-use system makes it simple to keep a sharp, perfectly angled cutting edge on bits you use often.',
+            },
+            {
+                title: 'Custom Point Angle Sharpening',
+                text: 'Set any angle from 115° to 140° with cast aluminum point angle shuttle for added durability, a unique BACK-CUT split-point bit an extra-narrow chisel point, making it faster to penetrate and less likely to wander on the material you are drilling in to.',
+            },
+            {
+                title: 'Extend The Life Of Your Bits',
+                text: 'From 3/32-inch to ¾-inch, create & remove split points & adjustable material take-off allows you to adjust the amount of material removed during sharpening; 6 foot power cord. Permanent magnet motor produces consistent power, regardless of speed or load.',
+            },
+            {
+                title: '3-Year Warranty',
+                text: 'Protects every bit sharpener against defects due to workmanship or design, using a 115V machine in a location with 230V power sources voids the warranty.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Submersible Pump',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Shallow-pan tank style designed for in-pan mounting.',
+            },
+            {
+                text: 'Bottom intake with 10 ft. maximum lift.',
+            },
+            {
+                text: 'Stainless steel shaft.',
+            },
+            {
+                text: '6 ft. power cord with 3-prong molded plug.',
+            },
+            {
+                text: 'Integral float-activated switch and thermal overload protection.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png', './img/img6-3.png', './img/img6-4.png'],
+    },
+]
